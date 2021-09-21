abb44e1a1fcf9786686b382c9e2db1826bdd06e9
camwhite <whiteceric@gmail.com>
2021-09-20 22:46:49 -0400

    Make Home section mobile friendly

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Link as ScrollTo} from "react-scroll";
+import { Link as ScrollTo } from "react-scroll";
 import PropTypes from 'prop-types';
+import useWindowDimensions from '../useWindowDimensions';
+import logo from "../../images/logo.png"
 import './Home.css'
 
-const Home = () => (
-  <div className="Home">
-    <div className="logo"></div>
-    <div className="logoTextContainer">
-      <h1 className="logoText">Booby's Kitchen</h1>
+const Home = () => {
+  const { height, width } = useWindowDimensions();
+  console.log(width)
+
+  return (
+    <div className="Home">
+      <div className="homeContents">
+        {(width > 1060) ? (
+          <>
+            <img src={logo} alt='' className="logo"></img>
+            <h1 className="logoText">Booby's Kitchen</h1>
+          </>
+        ) : (
+          <>
+            <h1 className="logoText">Booby's Kitchen</h1>
+            <img src={logo} alt='' className="logo"></img>
+          </>
+        )}
+        <div className="linkRow">
+          <ScrollTo to="about" spy={true} smooth={true} className="linkButton">About</ScrollTo>
+          <ScrollTo to="book" spy={true} smooth={true} className="linkButton">Book</ScrollTo>
+          <ScrollTo to="contact" spy={true} smooth={true} className="linkButton">Contact</ScrollTo>
+        </div>
+      </div>
     </div>
-    <div className="linkRow">
-      <ScrollTo to="about" spy={true} smooth={true} className="linkButton">About</ScrollTo>
-      <ScrollTo to="book" spy={true} smooth={true} className="linkButton">Book</ScrollTo>
-      <ScrollTo to="contact" spy={true} smooth={true} className="linkButton">Contact</ScrollTo>
-    </div>
-  </div>
-);
+  );
+}
 
 Home.propTypes = {};
 
