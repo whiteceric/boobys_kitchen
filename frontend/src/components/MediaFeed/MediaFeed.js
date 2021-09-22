e653052b5625c9e1584722435126d8520cff1bf7
camwhite <whiteceric@gmail.com>
2021-09-22 00:57:25 -0400

    Add photo slide show (WIP)

--- frontend/src/components/MediaFeed/MediaFeed.js
+++ frontend/src/components/MediaFeed/MediaFeed.js
@@ -2,11 +2,36 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './MediaFeed.css';
 
-const MediaFeed = () => (
-  <div className="MediaFeed">
-    MediaFeed Component
-  </div>
-);
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../../images/social_media', false, /\.(png|jpe?g|svg)$/));
+
+const MediaFeed = () => {
+  const [imgIndex, setImgIndex] = useState(0)
+  console.log(imgIndex)
+
+  useEffect(() => {
+    setInterval(() => {
+      let i = imgIndex + 1;
+      i = i % Object.keys(images).length;
+      setImgIndex(i);
+    }, 3000);
+  })
+
+  const changeImg = () => {
+    
+  }
+
+  return (
+    <div className="MediaFeed">
+      <img src={Object.entries(images)[imgIndex][1].default} alt='' />
+    </div>
+  );
+}
 
 MediaFeed.propTypes = {};
 
