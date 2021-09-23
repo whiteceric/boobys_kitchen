1405a47b3cff45cf1a30b21be1b667f0e0410fd1
camwhite <whiteceric@gmail.com>
2021-09-22 22:58:50 -0400

    Media Feed slide show

--- frontend/src/components/MediaFeed/MediaFeed.js
+++ frontend/src/components/MediaFeed/MediaFeed.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './MediaFeed.css';
+
+const delay = 2500;
 
 function importAll(r) {
   let images = {};
@@ -8,27 +10,55 @@ function importAll(r) {
   return images;
 }
 
-const images = importAll(require.context('../../images/social_media', false, /\.(png|jpe?g|svg)$/));
+const imagesObj = importAll(require.context('../../images/social_media', false, /\.(png|jpe?g|svg)$/));
+const images = [];
+for (let [k, v] of Object.entries(imagesObj)) {
+  images.push(v.default);
+}
 
 const MediaFeed = () => {
   const [imgIndex, setImgIndex] = useState(0)
-  console.log(imgIndex)
+  const timeoutRef = useRef(null);
+  console.log(images)
 
-  useEffect(() => {
-    setInterval(() => {
-      let i = imgIndex + 1;
-      i = i % Object.keys(images).length;
-      setImgIndex(i);
-    }, 3000);
-  })
-
-  const changeImg = () => {
-    
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
   }
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setImgIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [imgIndex]);
 
   return (
     <div className="MediaFeed">
-      <img src={Object.entries(images)[imgIndex][1].default} alt='' />
+      <a href="https://www.facebook.com/Boobys-Kitchen-229259131754242" target="_blank" rel="noopener noreferrer">
+        <h1 className="mediaTitle">Check Us Out!</h1>
+        <div className="slideShowContainer">
+          <div
+            className="feedContainer"
+            style={{ transform: `translate3d(${-imgIndex * 100}%, 0, 0)` }}>
+            {/* <img className="feedBottom" src={images[imgIndex+1]} alt='' />
+      <img className="feedTop" src={images[imgIndex]} alt='' /> */}
+            {images.map((img, index) => {
+              return (
+                <img className="mediaImage" src={img} alt='' key={index} />
+              )
+            })}
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
